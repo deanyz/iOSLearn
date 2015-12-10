de519a22bcdd7b8d4f336a277cd76fa9c3abaeaf
Rui Wang <411703186@qq.com>
2015-12-11 01:53:40 +0800

    add comment

--- Quiz/main.m
+++ Quiz/main.m
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-
+///git test xiaowang branch
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
